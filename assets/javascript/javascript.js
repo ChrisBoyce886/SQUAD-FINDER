bd14936277f620a6da6a6758553ae251493863ac
Boyce_Scout <49099741+ChrisBoyce886@users.noreply.github.com>
2019-06-10 19:25:17 -0400

    merge

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,33 +1,65 @@
-
-
 // ///////////////////////////////////////////////////////////////////////////////////////
 //                                     //GOOGLE API SECTION
 // //////////////////////////////////////////////////////////////////////////////////////
+
 //Geolocation 
-// var marker
-// var parkLocations = [
-//     {name: "FreedomPark", lat: 35.193978, lng: -80.842636}
-//     // [name: "Frazier Park, lat: 35.232251, lng: -80.858032"],
-//     // [name: "Revolution Park, lat: 35.214758, lng: -80.876093"],
-//     // ["Southside Park, 35.207150, -80.872784"],
-//     // ["Bryant Park, 35.227278, -80.870150"],
-//     // ["Kirk Farm Park, 35.321008, -80.731887"],
-//     // ["Nevin Community Park, 35.302511, -80.834128"],
-//     // ["Renaissance Park, 35.180768, -80.907574"]
-// ]
+var marker;
+var usermarker;
+var parkLocation = [
+    {name: "Freedom Park", lat: 35.193978, lng: -80.842636},
+    {name: "Frazier Park", lat: 35.232251, lng: -80.858032},
+    {name: "Revolution Park", lat: 35.214758, lng: -80.876093},
+    {name: "Southside Park", lat: 35.207150, lng: -80.872784},
+    {name: "Bryant Park", lat: 35.227278, lng: -80.870150},
+    {name: "Kirk Farm Park", lat: 35.321008, lng: -80.731887},
+    {name: "Nevin Community Park", lat: 35.302511, lng: -80.834128},
+    {name: "Renaissance Park", lat: 35.180768, lng: -80.90757}
+]
+
 function initMap() {
-  var mapCenter = {lat: 35.227085, lng: -80.843124}
+  var mapCenter = {lat: 35.227085, lng: -80.843124};
   map = new google.maps.Map(document.getElementById('google-maps-display'), {
     center: mapCenter,
     zoom: 11
   });   
 
-  // var marker = new google.maps.Marker({
-  //   position: parkLocations.lat.lng,
-  //   map: map,
-  //   title: "FreedomPark",
-  //   optimized: false
-  // }); 
+  
+  for (i=0;i < parkLocation.length; i++){
+   mapmarkerFinal = parkLocation[i];
+
+   var marker = new google.maps.Marker({   
+    position: mapmarkerFinal,
+    map: map,
+    icon: "assets/images/urbanpark.png",
+    title: mapmarkerFinal.name,
+    optimized: false,
+    draggable: false,
+    animation: google.maps.Animation.DROP,
+  }); 
+  //marker.addListener('click', toggleYelp); 
+}
+
+//function toggleYelp(){
+
+
+//}
+
+//Bounce animation for when event is clicked
+// function toggleBounce () {
+//   if (marker.getAnimation() != null) {
+//       marker.setAnimation(null);
+//   } else {
+//       marker.setAnimation(google.maps.Animation.BOUNCE);
+//   }
+// }
+
+// Add click listener to toggle bounce
+// google.maps.event.addListener(mapmarkerFinal, 'click', function () {
+//   toggleBounce();
+//   infowindow.open(map, mapmarkerFinal);
+//   setTimeout(toggleBounce, 1500);
+// });
+
 
 // Create the search box and link it to the UI element.
 
@@ -74,8 +106,8 @@ searchBox.addListener('places_changed', function() {
     // Create a marker for each place.
     markers.push(new google.maps.Marker({
       map: map,
-      icon: icon,
-      title: place.name,
+      icon: "assets/images/locationmarker.png",
+      title: "Your location",
       position: place.geometry.location
     }));
 
@@ -93,15 +125,25 @@ infoWindow = new google.maps.InfoWindow;
 
   //Try HTML5 geolocation.
   function geolocation (){
-  if (navigator.geolocation) {
+   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
 
+ var usermarker = new google.maps.Marker({   
+    position: pos,
+    map: map,
+    optimized: false,
+    icon: "assets/images/locationmarker.png",
+    title: "Your approx. location",
+    draggable: false,
+    animation: google.maps.Animation.DROP
+  });
+
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent('Approx. location found.');
       infoWindow.open(map);
       map.setCenter(pos);
     }, function() {
@@ -112,75 +154,52 @@ infoWindow = new google.maps.InfoWindow;
     handleLocationError(false, infoWindow, map.getCenter());
   }
 
-
+ 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
-                        'Error: We could not find your location.' :
+                        'Error: Geolocation request denied. \n Please enter you location below!' :
                         'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
 }
 
 }geolocation()
+
 }
 
-// function newLocation(newLat,newLng)
-// {
-// 	map.setCenter({
-// 		lat : newLat,
-// 		lng : newLng
-// 	});
-// }
+$(document).on("click", "#eventSearch", function () {
+      $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+    return false;
 
-// google.maps.event.addDomListener(window, 'load', initMap);
-
-// //Setting Location with jQuery
-// $(document).ready(function ()
-// {
-//     $("#parkLocation").on('click', function ()
-//     {
-// 	  newLocation(48.1293954,11.556663);
-// 	});
-
-// 	$("#2").on('click', function ()
-//     {
-// 	  newLocation(40.7033127,-73.979681);
-// 	});
-
-//     $("#3").on('click', function ()
-//     {
-// 	  newLocation(55.749792,37.632495);
-// 	});
-// });
-
-
-
-
-
-
-
-
-//Markers to create Hover-Over
-//   var marker = new google.maps.Marker({
-//     position: pos,
+// var eventmarker =    {
+//     position: parkLocation[i],
 //     map: map,
-//     draggable: true
-// });
+//     zoom: 15,
+//     optimized: false,
+//     icon: "assets/images/locationmarker.png",
+//     title: parkLocation.name,
+//     draggable: false,
+//     animation: google.maps.Animation.DROP
+// }
+// google.maps.event.addListener(marker, toggleBounce)
 
-// var searchBox = new google.maps.places.SearchBox(document.getElementById('location-SearchBox'));
+// function toggleBounce() {
 
-// google.maps.event.addDomListener(searchBox, 'places_changed', function() {
-//         var places = searchBox.getPlaces();
-//         var bounds = new google.maps.LatLngBounds();
-//         var i, place;
+//   if (marker.getAnimation() != null) {
+//     marker.setAnimation(null);
+//   } else {
+//     marker.setAnimation(google.maps.Animation.BOUNCE);
+//   }
+// }
+//  console.log($(this).children().first().text());
+ });
 
-//         for (i = 0; place = places[i]; i++) {
-//             bounds.extend(place.geometry.location);
-//             marker.setPosition(place.geometry.location);
-//         }
-//         map.fitBounds(bounds);
-//         map.setZoom(12);
-//     })
+ 
+ 
+
+
+
+
 // ///////////////////////////////////////////////////////////////////////////////////////
 //                                     //FIREBASE SECTION
 // //////////////////////////////////////////////////////////////////////////////////////
@@ -304,186 +323,186 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 ///////////////////////////////////////////////////////////////////////////////////////
                                     //FIREBASE SECTION
 //////////////////////////////////////////////////////////////////////////////////////
-window.onload = function(){
+// window.onload = function(){
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyCut4P2yrq2ECQWaX5liAQ6luwvuUQVozA",
-    authDomain: "project-1-14697.firebaseapp.com",
-    databaseURL: "https://project-1-14697.firebaseio.com",
-    projectId: "project-1-14697",
-    storageBucket: "project-1-14697.appspot.com",
-    messagingSenderId: "8693009592",
-    appId: "1:8693009592:web:3ffc100f48ce733c"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  let database = firebase.database();
-console.log("connected")
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyCut4P2yrq2ECQWaX5liAQ6luwvuUQVozA",
+//     authDomain: "project-1-14697.firebaseapp.com",
+//     databaseURL: "https://project-1-14697.firebaseio.com",
+//     projectId: "project-1-14697",
+//     storageBucket: "project-1-14697.appspot.com",
+//     messagingSenderId: "8693009592",
+//     appId: "1:8693009592:web:3ffc100f48ce733c"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   let database = firebase.database();
+// console.log("connected")
 
- $("#create").click(function(event) {
+//  $("#create").click(function(event) {
 
-  event.preventDefault()
+//   event.preventDefault()
 
 
-  // let users = firebase.database().child('users/')
-console.log("hey")
+//   // let users = firebase.database().child('users/')
+// console.log("hey")
 
-    let email = document.querySelector("#createUser")
-    let username = document.querySelector("#userName")
-    let password = document.querySelector("#createPassword")
+//     let email = document.querySelector("#createUser")
+//     let username = document.querySelector("#userName")
+//     let password = document.querySelector("#createPassword")
     
 
 
-firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-    .then(function(user) {
-        displayName = username.value // change this back to username.value
-        writeUserData(displayName, user) // possibly only need to pass displayName as parameter
-    })
+// firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+//     .then(function(user) {
+//         displayName = username.value // change this back to username.value
+//         writeUserData(displayName, user) // possibly only need to pass displayName as parameter
+//     })
 
 
-})
+// })
 
-//Firebase UI signin 
+// //Firebase UI signin 
 
-$("#submit").click(function(event) {
-  event.preventDefault();
+// $("#submit").click(function(event) {
+//   event.preventDefault();
   
-  console.log("button clicked")
-   let email = document.querySelector("#user")
-    let password = document.querySelector("#password")
+//   console.log("button clicked")
+//    let email = document.querySelector("#user")
+//     let password = document.querySelector("#password")
   
-  firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+//   firebase.auth().signInWithEmailAndPassword(email.value, password.value)
   
-    firebase.auth().onAuthStateChanged(user => {
+//     firebase.auth().onAuthStateChanged(user => {
 
-    if(user) {
-      window.location = 'main.html'; //After successful login, user will be redirected to main.html
-        }
+//     if(user) {
+//       window.location = 'main.html'; //After successful login, user will be redirected to main.html
+//         }
        
-});
+// });
   
-  })
+//   })
 
 
-function writeUserData(displayName, user) { //possibly only need to pass in displayName here
-    console.log("we're in")
-    firebase.database().ref('users/' + user.uid).set({
-        username: displayName,
-})
-
-
-
-$("#exampleInputEmail1").val("");
-$("#exampleUserName").val("");
-$("#exampleInputPassword1").val("");
-}
+// function writeUserData(displayName, user) { //possibly only need to pass in displayName here
+//     console.log("we're in")
+//     firebase.database().ref('users/' + user.uid).set({
+//         username: displayName,
+// })
 
 
 
-// MainPage Add Event:
-
-//converting Event Date and Time
- Date.prototype.toDatetimeLocal = 
-    function toDatetimeLocal() {
-      var 
-        date = this,
-        ten = function (i) {
-            return (i < 10 ? '0' : '') + i;
-        };
-        YYYY = date.getFullYear(),
-        MM = ten(date.getMonth() + 1),
-        DD = ten(date.getDate()),
-        HH = ten(date.getHours()),
-        II = ten(date.getMinutes()),
-        SS = ten(date.getSeconds())
-        ;
-        return YYYY + "-" + MM + "-" + DD + '' + HH + ':' + II + ':' + SS
-    }
-
-document.getElementById("addBtn").addEventListener("click", e => {
-
-    let user = firebase.auth().currentUser;  
-    console.log(user)  
-
-    // if(user)
-    //     console.log(db.collection("users").doc(user.uid))
-    // else
-    //     alert('user not logged in')
-
-  let DT = document.getElementById("DT")
-  let leaderName = document.querySelector("#squadLeader")
-  leaderName = leaderName.value
-  let eventDescription = document.querySelector("#eventDescription")
-  eventDescription = eventDescription.value
-  let eventLocation = document.querySelector("#location")
-  eventLocation = eventLocation.value
-  let eventName = document.querySelector("#inputEventName")
-  eventName = eventName.value
-  let eventRef = firebase.database().ref("events")
-  let newEventRef = eventRef.push();
-  let ISOString = new Date(DT.value).toISOString();
-  let finalTime = DT.value = new Date(ISOString).toDatetimeLocal();
-  let name = user.displayName
-  console.log(finalTime)
-  console.log(name)
-
-console.log(eventDescription)
+// $("#exampleInputEmail1").val("");
+// $("#exampleUserName").val("");
+// $("#exampleInputPassword1").val("");
+// }
 
 
-      newEventRef.set({
+
+// // MainPage Add Event:
+
+// //converting Event Date and Time
+//  Date.prototype.toDatetimeLocal = 
+//     function toDatetimeLocal() {
+//       var 
+//         date = this,
+//         ten = function (i) {
+//             return (i < 10 ? '0' : '') + i;
+//         };
+//         YYYY = date.getFullYear(),
+//         MM = ten(date.getMonth() + 1),
+//         DD = ten(date.getDate()),
+//         HH = ten(date.getHours()),
+//         II = ten(date.getMinutes()),
+//         SS = ten(date.getSeconds())
+//         ;
+//         return YYYY + "-" + MM + "-" + DD + '' + HH + ':' + II + ':' + SS
+//     }
+
+// document.getElementById("addBtn").addEventListener("click", e => {
+
+//     let user = firebase.auth().currentUser;  
+//     console.log(user)  
+
+//     // if(user)
+//     //     console.log(db.collection("users").doc(user.uid))
+//     // else
+//     //     alert('user not logged in')
+
+//   let DT = document.getElementById("DT")
+//   let leaderName = document.querySelector("#squadLeader")
+//   leaderName = leaderName.value
+//   let eventDescription = document.querySelector("#eventDescription")
+//   eventDescription = eventDescription.value
+//   let eventLocation = document.querySelector("#location")
+//   eventLocation = eventLocation.value
+//   let eventName = document.querySelector("#inputEventName")
+//   eventName = eventName.value
+//   let eventRef = firebase.database().ref("events")
+//   let newEventRef = eventRef.push();
+//   let ISOString = new Date(DT.value).toISOString();
+//   let finalTime = DT.value = new Date(ISOString).toDatetimeLocal();
+//   let name = user.displayName
+//   console.log(finalTime)
+//   console.log(name)
+
+// console.log(eventDescription)
+
+
+//       newEventRef.set({
       
-    leader: leaderName,
-    name: eventName,
-    eventDate: finalTime,
-    location: eventLocation,
-    description: eventDescription,
-    })
+//     leader: leaderName,
+//     name: eventName,
+//     eventDate: finalTime,
+//     location: eventLocation,
+//     description: eventDescription,
+//     })
 
-});
+// });
 
-// var eventRef = ref.child(key)
-let eventRef = firebase.database().ref("events")
-eventRef.on('child_added', function(childSnapshot){
+// // var eventRef = ref.child(key)
+// let eventRef = firebase.database().ref("events")
+// eventRef.on('child_added', function(childSnapshot){
 
   
-  let eventButton = $("<button>").addClass("eventButton")
-  let eventTitle = $("<p>").text(childSnapshot.val().name)
-  let eventLeader = $("<p>").text(childSnapshot.val().leader)
-  let eventDate = $("<p>").text(childSnapshot.val().eventDate)
-  let eventLocation = $("<p>").attr("id", "eventLocation").text(childSnapshot.val().location)
+//   let eventButton = $("<button>").addClass("eventButton")
+//   let eventTitle = $("<p>").text(childSnapshot.val().name)
+//   let eventLeader = $("<p>").text(childSnapshot.val().leader)
+//   let eventDate = $("<p>").text(childSnapshot.val().eventDate)
+//   let eventLocation = $("<p>").attr("id", "eventLocation").text(childSnapshot.val().location)
 
-  $(eventButton).append(eventLocation)
-  $(eventButton).append(eventDate)
-  $(eventButton).append(eventLeader)
-  $(eventButton).append(eventTitle)
+//   $(eventButton).append(eventLocation)
+//   $(eventButton).append(eventDate)
+//   $(eventButton).append(eventLeader)
+//   $(eventButton).append(eventTitle)
 
-  $("#event1").prepend(eventButton)
-
-
-
-
-
-})
+//   $("#event1").prepend(eventButton)
 
 
 
 
-firebase.auth().onAuthStateChanged(user => {
 
-  if(user) {
-console.log("you're logged in!")     
-}
+// })
+
+
+
+
+// firebase.auth().onAuthStateChanged(user => {
+
+//   if(user) {
+// console.log("you're logged in!")     
+// }
      
-});
+// });
 
 
-$(document).on("click", ".eventButton", function () {
+// $(document).on("click", ".eventButton", function () {
 
   
-  let parkLocation = document.getElementById("eventLocation")
+//   let parkLocation = document.getElementById("eventLocation")
   
-  console.log($(this).children().first().text());
-})
+//   console.log($(this).children().first().text());
+// })
 
-}
+// }
 
